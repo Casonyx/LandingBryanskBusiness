e482ce53e8ada233bd75f07a5118ff86232a0fe9
Casonyx <bogdancs-pro@yandex.ru>
2021-09-01 18:23:09 +0300

    version 1 Landing Business from Bryansk

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './js/rainbows.js';
+
+import './scss/mixins.scss';
+import './scss/normalize.scss';
+import './scss/variables.scss';
+import './scss/base.scss';
+import './scss/header.scss';
+import './scss/m-hero.scss';
+import './scss/ordering.scss';
+import './scss/custom-select.scss';
+import './scss/footer.scss';
---
+++ src/js/rainbows.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import 'jquery-custom-select';
+
+$(document).ready(function(){
+    //range
+    const rangeInputEl = document.getElementById('rangeInput');
+    const rangeValueEl = document.getElementById('rangeValue');
+
+    rangeValueEl.innerText = `${rangeInputEl.value} %`;
+
+    rangeInputEl.addEventListener('input', () => {
+        rangeValueEl.innerText = `${rangeInputEl.value} %`;
+    })
+
+    //select
+    $('.ordering__select .select').customSelect({
+        placeholder: '<span class="ordering__placeholder">Выберите тип системы</span>',
+        modifier: 'custom-select',
+        transition: 400,
+    });
+})
